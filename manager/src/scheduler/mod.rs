41ed70eb9e08667aab8f69fc2844a7a024dc747b
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2018-04-12 20:26:19 +0100

    WIP

--- manager/src/scheduler/mod.rs
+++ manager/src/scheduler/mod.rs
@@ -12,13 +12,14 @@ use tokio::net::TcpStream;
 use heracles_proto::datatypes::*;
 use settings::SETTINGS;
 use splitting;
+use broker::BrokerConnection;
+use state::State;
 
 /// Manages the entire data pipeline of the manager and links together all of the manager's
 /// components.
 pub struct Scheduler {
-    broker_channel: Channel<TcpStream>,
-    rx: mpsc::Receiver<Job>,
-    tx: mpsc::Sender<Job>,
+    broker: BrokerConnection,
+    state: State,
 }
 
 impl Scheduler {
@@ -26,13 +27,10 @@ impl Scheduler {
     ///
     /// Takes a handle to a [`heracles_manager_lib::broker::Broker`] which it uses to send
     /// [`Task`]s to workers for execution.
-    pub fn new(broker_channel: Channel<TcpStream>) -> Result<Self, Error> {
-        let (tx, rx) =
-            mpsc::channel::<Job>(SETTINGS.read().unwrap().get("scheduler.input_queue_size")?);
+    pub fn new(broker: BrokerConnection, store: State) -> Result<Self, Error> {
         Ok(Scheduler {
-            broker_channel,
-            rx,
-            tx,
+            broker,
+            store,
         })
     }
 
@@ -51,23 +49,13 @@ impl Scheduler {
     }
 
     fn process_task<'a>(&'a self, mut task: Task) -> impl Future<Item = Task, Error = Error> + 'a {
-        // This unwrap should be safe as we set a default for this value.
-        let queue_name = SETTINGS.read().unwrap().get("broker_queue_name").unwrap();
-        let task_id = task.get_id().to_string();
-        lazy(|| done(task.write_to_bytes()))
-            .map_err(move |e| e.context(SchedulerError::TaskSerialisationFailure { task_id }))
+        task.set_time_started(Utc::now().timestamp() as u64);
+        task.set_status(TaskStatus::TASK_IN_PROGRESS);
+        self.state.save_task(task);
+
+        self.broker.send(task)
+            .map_err(|e| e.context(BrokerSendFailure))
             .from_err()
-            .and_then(move |bytes| {
-                self.broker_channel
-                    .basic_publish(
-                        "",
-                        queue_name,
-                        &bytes,
-                        &BasicPublishOptions::default(),
-                        BasicProperties::default(),
-                    )
-                    .from_err()
-            })
             .and_then(|ack| {
                 if let Some(completed) = ack {
                     if completed {
@@ -77,10 +65,11 @@ impl Scheduler {
                     }
                 } else {
                     task.set_status(TaskStatus::TASK_UNKNOWN);
-                    panic!("Queue was not confirm queue. This should not happen.");
+                    panic!("ack of task failed. this should not happen");
                 }
                 task.set_time_done(Utc::now().timestamp() as u64);
-                future::ok(task)
+                self.state.save_task(task);
+                future::ok(task);
             })
     }
 }
@@ -89,4 +78,6 @@ impl Scheduler {
 pub enum SchedulerError {
     #[fail(display = "failed to serialise task with id {}", task_id)]
     TaskSerialisationFailure { task_id: String },
+    #[fail(display = "failed to send task to broker")]
+    BrokerSendFailure,
 }
