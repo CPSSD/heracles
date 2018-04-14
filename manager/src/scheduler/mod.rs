59a10b87c3a3f61eb21116fd36ac395dce5f2929
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2018-04-14 15:50:42 +0100

    WIP

--- manager/src/scheduler/mod.rs
+++ manager/src/scheduler/mod.rs
@@ -38,28 +38,24 @@ impl Scheduler {
     }
 
     fn process_job<'a>(&'a self, job: Job) -> impl Future<Item = Job, Error = Error> + 'a {
-        // lazy(|| done(splitting::map::split(&job)))
-        //     .map_err(|e| e.context(SchedulerError::MapSplitFailure))
-        //     .from_err()
-        //     .and_then(|tasks| {
-
-        //         future::join_all(tasks.as_slice().iter().map(|task| {
-        //             self.process_task(task)
-        //         }))
-        //         .and_then(|_| {
-        //         // TODO: Run reduce
-        //         future::ok(job)
-        //         })
-        //     })
-
-        // lazy(|| done(splitting::map::split(&job)))
-        //     .and_then(|_| {
-        //         future::ok(job)
-        //     })
-        future::ok(job)
+        lazy(|| done(splitting::map::split(&job)))
+            .and_then(|tasks| self.run_tasks(tasks))
+            .and_then(move |_| future::ok(splitting::reduce::split(&job)))
+            .and_then(|tasks| self.run_tasks(tasks))
+            .and_then(move |_| {
+                // mark job as done
+                future::ok(job)
+            })
     }
 
-    fn process_task<'a>(&'a self, mut task: Task) -> impl Future<Item = Task, Error = Error> + 'a {
+    fn run_tasks<'a>(&self, tasks: Vec<Task>) -> impl Future<Item = (), Error = Error> + 'a {
+        future::join_all(tasks.iter().map(|task| self.process_task(task)))
+            .and_then(|_| {
+                future::ok(())
+            })
+    }
+
+    fn process_task<'a>(&'a self, mut task: &'a Task) -> impl Future<Item = (), Error = Error> + 'a {
         task.set_time_started(Utc::now().timestamp() as u64);
         task.set_status(TaskStatus::TASK_IN_PROGRESS);
         self.store.save_task(&task);
@@ -80,7 +76,7 @@ impl Scheduler {
                 }
                 task.set_time_done(Utc::now().timestamp() as u64);
                 self.store.save_task(&task);
-                Ok(task)
+                future::ok(())
             })
     }
 }
