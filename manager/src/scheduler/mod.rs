1c8dbbd4190cf97f24da8344e11661b555b01395
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2018-04-16 21:27:54 +0100

    WIP

--- manager/src/scheduler/mod.rs
+++ manager/src/scheduler/mod.rs
@@ -16,6 +16,7 @@ use settings::SETTINGS;
 
 /// Manages the entire data pipeline of the manager and links together all of the manager's
 /// components.
+#[derive(Clone)]
 pub struct Scheduler {
     broker: Arc<BrokerConnection + Sync + Send>,
     store: Arc<State + Sync + Send>,
@@ -39,50 +40,56 @@ impl Scheduler {
         })
     }
 
-    pub fn schedule(&self, req: Job) -> Result<String, SchedulerError> {
+    pub fn schedule(self, req: Job) -> Result<String, SchedulerError> {
+        let sch = self.clone();
+
         let mut job = req.clone();
 
         let id = Uuid::new_v4().to_string();
         job.set_id(id.clone());
         // TODO: Scheduling time
 
-        self.store.save_job(&job.clone()).unwrap();
+        sch.store.save_job(&job.clone()).unwrap();
 
         // self.tx.borrow_mut().take().unwrap().send(job.clone());
 
-        self.tx.lock().unwrap().take().unwrap().clone().send(job.clone());
+        sch.tx.lock().unwrap().take().unwrap().clone().send(job.clone());
 
         Ok(id)
     }
 
-    pub fn cancel(&self, _job_id: &str) -> Result<(), SchedulerError> {
+    pub fn cancel(self, _job_id: &str) -> Result<(), SchedulerError> {
         unimplemented!()
     }
 
-    pub fn run(&self) -> impl Future<Item = (), Error = ()> {
-        self.rx
+    pub fn run(self) -> impl Future<Item = (), Error = ()> {
+        let sch = self.clone();
+
+        sch.rx
             .clone()
             .lock()
             .unwrap()
             .take()
             .unwrap()
             .map_err(|_| unreachable!("should never happen"))
-            .for_each(|job| self.process_job(job))
+            .for_each(|job| sch.process_job(job))
             .map_err(|_| panic!("should not happen"))
     }
 
-    fn process_job(&self, job: Job) -> impl Future<Item = (), Error = Error> + 'static {
+    fn process_job(self, job: Job) -> impl Future<Item = (), Error = Error> + 'static {
+        let sch = self.clone();
+
         // TODO: Refactor this ugly code. This should not be cloned so many times.
         let job1 = job.clone();
         let job2 = job.clone();
         let job3 = job.clone();
 
-        let store = self.store.clone();
-        lazy(|| done(splitting::map::split(&job1)))
-            .and_then(|tasks| self.run_tasks(tasks))
-            .and_then(|_| future::ok(splitting::reduce::split(&job2)))
-            .and_then(|tasks| self.run_tasks(tasks))
-            .and_then(|_| {
+        let store = sch.store.clone();
+        lazy(move || done(splitting::map::split(&job1)))
+            .and_then(move |tasks| sch.run_tasks(tasks))
+            .and_then(move |_| future::ok(splitting::reduce::split(&job2)))
+            .and_then(move |tasks| sch.run_tasks(tasks))
+            .and_then(move |_| {
                 // mark job as done
 
                 store.save_job(&job3);
@@ -90,28 +97,32 @@ impl Scheduler {
             })
     }
 
-    fn run_tasks(&self, tasks: Vec<Task>) -> impl Future<Item = (), Error = Error> + 'static {
+    fn run_tasks(self, tasks: Vec<Task>) -> impl Future<Item = (), Error = Error> + 'static {
+        let sch = self.clone();
+
         // Normally we would do `.into_iter()` on the task, but it looks like there is a problem
         // with it currently. This issue describes the error we are having:
         //      https://github.com/rust-lang/rust/issues/49926
         let mut task_futures = vec![];
         for mut task in tasks {
-            task_futures.push(self.process_task(task.clone()));
+            task_futures.push(sch.process_task(task.clone()));
         }
         future::join_all(task_futures).and_then(|_| future::ok(()))
     }
 
     fn process_task(&self, mut task: Task) -> impl Future<Item = (), Error = Error> + 'static {
+        let sch = self.clone();
+
         task.set_time_started(Utc::now().timestamp() as u64);
         task.set_status(TaskStatus::TASK_IN_PROGRESS);
 
-        let store = self.store.clone();
+        let store = sch.store.clone();
         store.save_task(&task);
 
-        self.broker.send(task.clone())
+        sch.broker.send(task.clone())
             // .map_err(|e| e.context(SchedulerError::BrokerSendFailure))
             // .from_err()
-            .and_then(|ack| {
+            .and_then(move |ack| {
                 if let Some(completed) = ack {
                     if completed {
                         task.set_status(TaskStatus::TASK_DONE);
@@ -129,19 +140,6 @@ impl Scheduler {
     }
 }
 
-// fn run_tasks(tasks: Vec<Task>) -> impl Future<Item = (), Error = Error> {
-//     // Normally we would do `.into_iter()` on the task, but it looks like there is a problem
-//     // with it currently. This issue describes the error we are having:
-//     //      https://github.com/rust-lang/rust/issues/49926
-//     let mut task_futures = vec![];
-//     for mut task in tasks {
-//         task_futures.push(self.process_task(task.clone()));
-//     }
-//     future::join_all(task_futures).and_then(|_| future::ok(()))
-// }
-
-// fn process_task()
-
 #[derive(Debug, Fail)]
 pub enum SchedulerError {
     #[fail(display = "failed to split job into map tasks")]
@@ -151,3 +149,61 @@ pub enum SchedulerError {
     #[fail(display = "error receiving")]
     RxFailure,
 }
+
+
+// fn process_job(&job: Job) -> impl Future<Item = (), Error = Error> + 'static {
+//     // TODO: Refactor this ugly code. This should not be cloned so many times.
+//     let job1 = job.clone();
+//     let job2 = job.clone();
+//     let job3 = job.clone();
+
+//     let store = self.store.clone();
+//     let broker = self.broker.clone();
+//     lazy(|| done(splitting::map::split(&job1)))
+//         .and_then(|tasks| run_tasks(tasks, broker, store))
+//         .and_then(|_| future::ok(splitting::reduce::split(&job2)))
+//         .and_then(|tasks| run_tasks(tasks, broker, store))
+//         .and_then(|_| {
+//             // mark job as done
+
+//             store.save_job(&job3);
+//             future::ok(())
+//         })
+//  }
+
+// fn run_tasks(tasks: Vec<Task>, broker: Arc<BrokerConnection>, store: Arc<State>) -> impl Future<Item = (), Error = Error> {
+//     // Normally we would do `.into_iter()` on the task, but it looks like there is a problem
+//     // with it currently. This issue describes the error we are having:
+//     //      https://github.com/rust-lang/rust/issues/49926
+//     let mut task_futures = vec![];
+//     for mut task in tasks {
+//         task_futures.push(process_task(task.clone(), broker, store));
+//     }
+//     future::join_all(task_futures).and_then(|_| future::ok(()))
+// }
+
+// fn process_task(mut task: Task, broker: Arc<BrokerConnection>, store: Arc<State>) -> impl Future<Item = (), Error = Error> + 'static {
+//     task.set_time_started(Utc::now().timestamp() as u64);
+//     task.set_status(TaskStatus::TASK_IN_PROGRESS);
+
+//     store.save_task(&task);
+
+//     broker.send(task.clone())
+//         // .map_err(|e| e.context(SchedulerError::BrokerSendFailure))
+//         // .from_err()
+//         .and_then(|ack| {
+//             if let Some(completed) = ack {
+//                 if completed {
+//                     task.set_status(TaskStatus::TASK_DONE);
+//                 } else {
+//                     task.set_status(TaskStatus::TASK_FAILED);
+//                 }
+//             } else {
+//                 task.set_status(TaskStatus::TASK_UNKNOWN);
+//                 panic!("ack of task failed. this should not happen");
+//             }
+//             task.set_time_done(Utc::now().timestamp() as u64);
+//             store.save_task(&task);
+//             future::ok(())
+//         })
+// }
